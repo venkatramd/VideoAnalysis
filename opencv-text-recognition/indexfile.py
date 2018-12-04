39b5797574b8ba430ede73f5ad1999af498bc0b9
testuser <testuser@example.com>
2018-12-04 19:17:05 +0530

    all individual modules are set

--- opencv-text-recognition/indexfile.py
+++ opencv-text-recognition/indexfile.py
@@ -1,9 +1,10 @@
 import os
-os.system("python text_recognition.py --east frozen_east_text_detection.pb --image images/example_02.jpg")
-files = []
+allframes = []
 cwd = os.getcwd()+"/images/"
 print(cwd)
 for i in os.listdir(cwd):
     if i.endswith('.jpg') or i.endswith('.png'):
-        files.append(i)
-print(files)
+        allframes.append(i)
+print(allframes)
+for image in (allframes):
+    os.system("python text_recognition.py --east frozen_east_text_detection.pb --image images/"+image+"")
---
+++ tesseract-python/tesseractwithopencv.py
@@ -0,0 +1,2 @@
+import os
+os.system("python ocr.py --image images/video2.jpg")
