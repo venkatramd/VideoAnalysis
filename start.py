acfeed864cc0bac51e682c190d8ff2c886235540
testuser <testuser@example.com>
2018-12-10 19:34:17 +0530

    WORDCLOUD is done , now starting with parallel processing

---
+++ start.py
@@ -0,0 +1,3 @@
+import os
+os.system("python /home/venkatram/PycharmProjects/VidoAnalysis/imagetoframes/imagetoframes.py")
+os.system("python /home/venkatram/PycharmProjects/VidoAnalysis/indexfile.py")
